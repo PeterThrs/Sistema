a9cadf48fd4a2c6f568e1e0484a66ab2e6d58d83
peter <peterlopezthrs@gmail.com>
2023-02-28 18:29:52 -0600

    Subiendo el proyecto

---
+++ SistemaVentas/src/main/java/com/thors/sistemaventas/SistemaVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thors.sistemaventas;
+
+/**
+ *
+ * @author Root
+ */
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

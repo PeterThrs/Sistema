c4836c7db3ca2eb09235470a3a6c464a6c6746d6
Juanjo Lopez <juanlr6@gmail.com>
2023-03-02 12:02:19 -0600

    Agregué el login y listar

---
+++ SistemaVentas/src/main/java/Principal/Ejecucion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Cajero.ModificarListaUsuarios;
+
+/**
+ *
+ * @author juanj
+ */
+public class Ejecucion {
+
+    public static void main(String[] args) {
+        Login login=new Login();
+        login.setVisible(true);
+    }
+    
+}
--- SistemaVentas/src/main/java/com/ventas/administrador/PrincipalAdministrador.java
+++ SistemaVentas/src/main/java/com/ventas/administrador/PrincipalAdministrador.java
@@ -1,5 +1,7 @@
 package com.ventas.administrador;
 
+import Cajero.ModificarListaUsuarios;
+
 public class PrincipalAdministrador extends javax.swing.JFrame {
 
     public PrincipalAdministrador() {
@@ -64,6 +66,11 @@ public class PrincipalAdministrador extends javax.swing.JFrame {
         jMenu1.add(jMenuItem1);
 
         jMenuItem2.setText("Listar Usuario");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
@@ -140,6 +147,11 @@ public class PrincipalAdministrador extends javax.swing.JFrame {
         AgregarProducto AP = new AgregarProducto();
         AP.setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        ModificarListaUsuarios modi=new ModificarListaUsuarios(this, true);
+        modi.setVisible(true);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */

2029614afc910147fdaa1f0f5a65aa61aab67499
peter <peterlopezthrs@gmail.com>
2023-03-02 23:12:24 -0600

    Probando con la libreria JTatto para darle una mejor intefaz al programa

--- SistemaVentas/src/main/java/Principal/Ejecucion.java
+++ SistemaVentas/src/main/java/Principal/Ejecucion.java
@@ -7,6 +7,7 @@ public class Ejecucion {
     public static void main(String[] args) {
         Login login=new Login();
         login.setVisible(true);
+        
     }
     
 }
--- SistemaVentas/src/main/java/Principal/Login.java
+++ SistemaVentas/src/main/java/Principal/Login.java
@@ -5,6 +5,10 @@
 package Principal;
 
 import com.ventas.administrador.PrincipalAdministrador;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -16,6 +20,18 @@ public class Login extends javax.swing.JFrame {
      * Creates new form Market
      */
     public Login() {
+        try {
+            UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+            //UIManager.setLookAndFeel("com.jtattoo.plaf.texture.patterns.textile");
+        } catch (ClassNotFoundException ex) {
+            //Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            //Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            //Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            //Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+        }
         initComponents();
         this.setLocationRelativeTo(null);
     }

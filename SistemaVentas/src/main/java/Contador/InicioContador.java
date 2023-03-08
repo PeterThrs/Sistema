c340c1a5b1bfaf5d709ecd3b7da687035e9dc9e9
EdRyz <anaylalo60@gmail.com>
2023-03-08 08:33:10 -0600

    Agregando las ventanas Dialog de Contador

--- SistemaVentas/src/main/java/Contador/InicioContador.java
+++ SistemaVentas/src/main/java/Contador/InicioContador.java
@@ -19,6 +19,7 @@ public class InicioContador extends javax.swing.JFrame {
     public InicioContador() {
         initComponents();
         this.setLocationRelativeTo(null);
+        this.setExtendedState(this.MAXIMIZED_BOTH);
     }
 
     
@@ -30,14 +31,15 @@ public class InicioContador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu3 = new javax.swing.JMenu();
         btnSalir = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        txtUsuario = new javax.swing.JTextField();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
-        menuReporteVenta = new javax.swing.JMenuItem();
-        menuReporteInversion = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+
+        jMenu3.setText("jMenu3");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,36 +50,18 @@ public class InicioContador extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Usuario");
-
-        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtUsuarioActionPerformed(evt);
-            }
-        });
-
         jMenu1.setText("Opciones");
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Acciones");
+        jMenu4.setText("Acciones");
 
-        menuReporteVenta.setText("Reporte de Ventas");
-        menuReporteVenta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuReporteVentaActionPerformed(evt);
-            }
-        });
-        jMenu2.add(menuReporteVenta);
+        jMenuItem1.setText("Reporte de Ventas");
+        jMenu4.add(jMenuItem1);
 
-        menuReporteInversion.setText("Reporte de Inversiones");
-        menuReporteInversion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuReporteInversionActionPerformed(evt);
-            }
-        });
-        jMenu2.add(menuReporteInversion);
+        jMenuItem2.setText("Reporte de Inversiones");
+        jMenu4.add(jMenuItem2);
 
-        jMenuBar1.add(jMenu2);
+        jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
 
@@ -86,24 +70,14 @@ public class InicioContador extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(369, Short.MAX_VALUE)
                 .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(243, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
+                .addContainerGap(218, Short.MAX_VALUE)
                 .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -112,32 +86,16 @@ public class InicioContador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
-        Login login = new Login(); 
-        login.setVisible(true); 
         dispose();
     }//GEN-LAST:event_btnSalirActionPerformed
 
-    private void menuReporteVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuReporteVentaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_menuReporteVentaActionPerformed
-
-    private void menuReporteInversionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuReporteInversionActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_menuReporteInversionActionPerformed
-
-    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
-        // TODO add your handling code here:
-        txtUsuario.setText("Nombre del Usuario");
-    }//GEN-LAST:event_txtUsuarioActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSalir;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenu menuReporteInversion;
-    private javax.swing.JMenuItem menuReporteVenta;
-    private javax.swing.JTextField txtUsuario;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }

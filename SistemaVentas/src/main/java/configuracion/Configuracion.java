03770ea9c603628246deb8d6ead3880507ad9de0
peter <peterlopezthrs@gmail.com>
2023-03-08 22:38:39 -0600

    Agregando comentarios

--- SistemaVentas/src/main/java/configuracion/Configuracion.java
+++ SistemaVentas/src/main/java/configuracion/Configuracion.java
@@ -8,26 +8,26 @@ import javax.swing.JFrame;
 
 public class Configuracion {
     
-    
+    //configuraciones generales para una ventana de tipo JFrame
     public static <T extends JFrame> void styles(T t){
         t.setExtendedState(t.MAXIMIZED_BOTH);
         t.setTitle(Sesion.app.getAppNombre());
         t.setLocationRelativeTo(null);
         t.setIconImage(Sesion.app.getIcono());
     }
-    
+    //configuraciones generales par auna ventana de tipo JDialog
     public static <T extends JDialog> void styles(T t){
         t.setTitle(Sesion.app.getAppNombre());
         t.setLocationRelativeTo(null);
         t.setIconImage(Sesion.app.getIcono());
     }
-    
+    //configuraicones para el tipo de letra general de la aplicacion
     public static <T extends JComponent> void normal(T... t){
         for(int i = 0; i < t.length; i++){
-            t[i].setFont(new Font("Verdana", Font.PLAIN, 13));
+            t[i].setFont(new Font("Roboto", Font.PLAIN, 13));
         } 
     }
-    
+    //configuraicones para el tipo de titulos general de la aplicacion
     public static <T extends JComponent> void titulo(T... t){
         for(int i = 0; i < t.length; i++){
             t[i].setFont(new Font("Arial", Font.BOLD, 16));

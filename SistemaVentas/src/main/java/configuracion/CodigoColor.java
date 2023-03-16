ed6a46feb04237ef9ff362b1a315da490409da34
peter <peterlopezthrs@gmail.com>
2023-03-16 08:37:21 -0600

    Terminando una parte de los colores para todas las ventanas

--- SistemaVentas/src/main/java/configuracion/CodigoColor.java
+++ SistemaVentas/src/main/java/configuracion/CodigoColor.java
@@ -7,34 +7,36 @@ import java.awt.Color;
  * @author Root
  */
 public class CodigoColor {
+    
+    //-> Colores de Peter 
     /**
-     * Este es un color blanco crema
+     * Este es un color blanco-gris
      */
-    public static Color cFondo = new Color(233, 216, 166);
+    public static Color cFondo = new Color(255, 255, 255);
     /**
      * Este es un color azul obscuro 
      */
-    public static Color cLetrasObscuras = new Color(0, 18, 25);
+    public static Color cLetrasObscuras = new Color(83, 82, 237);
     /**
      * Este es un color azul intermeido
      */
-    public static Color cLetrasClaras = new Color(233, 216, 166);
+    public static Color cLetrasClaras = new Color(255, 255, 255);
     /**
      * Este es un color azul claro
      */
-    public static Color cLetrasTitulo = new Color(155, 34, 38);
+    public static Color cLetrasTitulo = new Color(47, 53, 66);
     /**
      * Este se un color Rojo Obscuro (ginda) 
      */
-    public static Color cSeparadores = new Color(0, 95, 115);
+    public static Color cSeparadores = new Color(255, 107, 129);
     /**
      * Este es un rojo intermedio (rojizo) 
      */
-    public static Color cFondoBtn = new Color(0, 18, 25);
+    public static Color cFondoBtn = new Color(112, 161, 255);
     /**
      * NO me acuerdo 
      */
-    public static Color cLetrasBtn = new Color(233, 216, 166);
+    public static Color cLetrasBtn = new Color(255, 255, 255);
     /**
      * NO me acuerdo 
      */
@@ -43,4 +45,15 @@ public class CodigoColor {
      * NO me acuerdo 
      */
     public static Color cLetrasLabel = new Color(155, 34, 38);
+    
+    
+    //-> Colores de Juan
+    //listar
+    //
+    
+    //-> Colores de Jorge
+    
+    
+    //-> Colores de 
+    
 }

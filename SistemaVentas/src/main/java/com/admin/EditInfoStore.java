e127137c89cb4696e501642d296453bbb298480f
EdRyz <anaylalo60@gmail.com>
2023-05-22 08:55:29 -0600

    Modificando pequeños cambios

--- SistemaVentas/src/main/java/com/admin/EditInfoStore.java
+++ SistemaVentas/src/main/java/com/admin/EditInfoStore.java
@@ -186,9 +186,12 @@ public class EditInfoStore extends javax.swing.JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 12;
-        gridBagConstraints.gridwidth = 9;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(7, 230, 0, 0);
+        gridBagConstraints.gridwidth = 8;
+        gridBagConstraints.gridheight = 3;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(7, 221, 0, 0);
         add(jPhone2, gridBagConstraints);
 
         jCorreoE.setText("Correo electronico:");
